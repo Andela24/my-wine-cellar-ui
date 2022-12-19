825418c9e1816a2b16273b368ccc2203a7798a84
Andela Stevanovic <andjela.s166@gmail.com>
2022-12-19 14:32:19 -0500

    user contex debbuged

--- src/context/user_context.js
+++ src/context/user_context.js
@@ -1,12 +1,46 @@
-import { createContext, useState } from "react"
+import { createContext, useState, useEffect } from "react"
 
 const UserContext = createContext('User')
 
 function UserProvider({children}) {
   const [currentUser, setCurrentUser] = useState(null);
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [errors, setErrors] = useState(false)
+  const [loading, setLoading]= useState(true);
+
+  useEffect(()=> {
+    fetch('/me')
+    .then(resp => {
+        if(resp.ok){
+            resp.json().then(data => {
+               setCurrentUser(data)
+               data.error? setLoggedIn(false) : setLoggedIn(true)
+            })
+        }else {
+            resp.json().then(data => setErrors(data.error))
+        }
+    })
+   
+  }, [])
+
+  const handleCurrentUser = (user) => {
+    if(user.username) {
+      setCurrentUser(user);
+      setLoggedIn(true);
+      setLoading(false);
+   
+    }
+  }
+
+  const logoutCurrentUser = () => {
+    setCurrentUser({});
+    setLoggedIn(false);
+    setLoading(false);
+  }
+
 
   return(
-    <UserContext.Provider value={{currentUser, setCurrentUser}}>{children}</UserContext.Provider>
+    <UserContext.Provider value={{currentUser, setCurrentUser, handleCurrentUser, loggedIn, errors, loading, logoutCurrentUser}}>{children}</UserContext.Provider>
   )
 }
 
